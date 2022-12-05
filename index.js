e52ef433e9033d2e90a572f0f2fcb99151570fba
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2022-12-05 11:57:39 +0300

    Identifying animal by ID

--- index.js
+++ index.js
@@ -80,25 +80,69 @@
 // }
 
 
-document.addEventListener('DOMContentLoaded', (_)=>{
-let charactersUrl = "http://localhost:3000/characters"
+// document.addEventListener('DOMContentLoaded', (_)=>{
+// let charactersUrl = "http://localhost:3000/characters"
 
-async function getAnimals(){
+// async function getAnimals(){
 
-    const res = await fetch(charactersUrl)
-    const data = await res.json()
+//     const res = await fetch(charactersUrl)
+//     const data = await res.json()
     
-    return data
+//     return data
+// }
+
+
+// const chars = getAnimals()
+
+
+
+
+
+
+
+
+// get input
+
+// function findCHARACTERbyName(characters){
+//     // const inputValue = document.querySelector('#id').value
+//     let inputValue = document.querySelector('#animal-name').value
+
+//     let oneChar =  characters.find(char => char.name === inputValue).createElement('li');
+//     return oneChar
+    
+//     document.querySelector('#thy-animals').appendChild(oneChar);
+// }
+
+
+// //  //Getting one name from the list 
+//  function findByName (name) {
+   
+//     return name.find(item = item.name === value)
+//  }
+
+
+// // const names = [{name: 'one', num: 2}, {name: 'two', num: 3},]
+
+
+
+// // console.log(findByName(names))
+
+// })
+
+const init =()=>{
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (e)=>{
+        e.preventDefault();
+
+        const input = document.querySelector('input#animal-name');
+
+        fetch(`http://localhost:3000/characters/${input.value}`)
+        .then(response => response.json())
+        .then(
+            data=>{
+                console.log(data);
+            }
+        )
+    })  
 }
-
-
-let chars = getAnimals()
-
-let value = document.querySelector('#animal-name').value
-
-
-let oneChar = chars.find(char => char.name === value).createElement('li')
-document.querySelector('#thy-animals').appendChild(oneChar);
-
-
-})
+init()
